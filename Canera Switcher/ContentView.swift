6576b7a5e4000d2e5f9956890782c7c9aaa61c21
David Beck <code@davidbeck.co>
2020-08-04 13:42:51 -0700

    Initial Commit

---
+++ Canera Switcher/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Canera Switcher
+//
+//  Created by David Beck on 8/4/20.
+//  Copyright © 2020 David Beck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
